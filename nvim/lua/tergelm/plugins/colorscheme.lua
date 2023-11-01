9e2564a6383d4966a59acf32532d6c918682f01a
Tergel Munkhdelger <tergelm@gmail.com>
2023-11-01 10:09:12 +0800

    tweaked colorscheme for opacity term

--- nvim/lua/tergelm/plugins/colorscheme.lua
+++ nvim/lua/tergelm/plugins/colorscheme.lua
@@ -3,6 +3,18 @@ return {
 	name = "rose-pine",
 	priority = 1000,
 	config = function()
+		require("rose-pine").setup({
+			disable_float_background = true,
+			disable_background = true,
+			dim_nc_background = false,
+			highlight_groups = {
+				TelescopeNormal = { bg = "none" },
+				TelescopeBorder = { bg = "none" },
+				TelescopeTitle = { bg = "none", fg = "iris" },
+				TelescopePreviewTitle = { bg = "iris", fg = "base" },
+				TelescopeResultsTitle = { fg = "iris" },
+			},
+		})
 		vim.cmd([[colorscheme rose-pine]])
 	end,
 }
