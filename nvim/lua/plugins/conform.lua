4a8c478161ed83681ac536a33e35c6dc775cb3a6
Tergel M <tergel.m@cody.mn>
2025-05-13 11:04:16 +0800

    revert: lsp config binds

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,21 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" },
-	cmd = "ConformInfo",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-		},
-	},
-	keys = {
-		{
-			"<leader>fm",
-			function()
-				require("conform").format({ async = false, quiet = false, lsp_format = "fallback", timeout_ms = 3000 })
-			end,
-			mode = { "n", "v" },
-			desc = "Format Injected Langs",
-		},
-	},
+    "stevearc/conform.nvim",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = "ConformInfo",
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            json = { "prettier" },
+            jsonc = { "prettier" },
+            graphql = { "prettier" }
+        },
+    },
+    keys = {
+        {
+            "<leader>fm",
+            function()
+                require("conform").format({ async = false, quiet = false, lsp_format = "fallback", timeout_ms = 3000 })
+            end,
+            mode = { "n", "v" },
+            desc = "Format Injected Langs",
+        },
+    },
 }
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,45 +1,46 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = { "saghen/blink.cmp" },
+    "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
 
-	opts = {
-		servers = {
-			lua_ls = {},
-			cssls = {},
-			html = {},
-			tailwindcss = {},
-			ts_ls = {},
-		},
-	},
+    opts = {
+        servers = {
+            lua_ls = {},
+            cssls = {},
+            html = {},
+            tailwindcss = {},
+            ts_ls = {},
+            graphql = {},
+        },
+    },
 
-	config = function(_, opts)
-		local lspconfig = require("lspconfig")
+    config = function(_, opts)
+        local lspconfig = require("lspconfig")
 
-		local on_attach = function(_, bufnr)
-			local function map(mode, lhs, rhs, desc)
-				vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-			end
+        local on_attach = function(_, bufnr)
+            local function map(mode, lhs, rhs, desc)
+                vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+            end
 
-			map("n", "K", vim.lsp.buf.hover, "Hover info")
-			map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-			map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-			map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-			map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
-			map("n", "gr", vim.lsp.buf.references, "Find references")
-			map("n", "gs", vim.lsp.buf.signature_help, "Signature help")
-			map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-			map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
-			map("n", "g?", function()
-				vim.diagnostic.open_float(0)
-			end, "Show diagnostics in float")
-		end
+            map("n", "K", vim.lsp.buf.hover, "Hover info")
+            map("n", "gd", "<cmd>Telescope lsp_definitions<cr>", "Go to definition")
+            map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+            map("n", "gi", "<cmd>Telescope lsp_implementations<cr>", "Go to implementation")
+            map("n", "gt", "<cmd>Telescope lsp_type_definitions<cr>", "Go to type definition")
+            map("n", "gr", "<cmd>Telescope lsp_references<cr>", "Find references")
+            map("n", "gs", vim.lsp.buf.signature_help, "Signature help")
+            map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+            map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+            map("n", "g?", function()
+                vim.diagnostic.open_float(0)
+            end, "Show diagnostics in float")
+        end
 
-		for server, config in pairs(opts.servers) do
-			-- passing config.capabilities to blink.cmp merges with the capabilities in your
-			-- `opts[server].capabilities, if you've defined it
-			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-			config.on_attach = on_attach
-			lspconfig[server].setup(config)
-		end
-	end,
+        for server, config in pairs(opts.servers) do
+            -- passing config.capabilities to blink.cmp merges with the capabilities in your
+            -- `opts[server].capabilities, if you've defined it
+            config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+            config.on_attach = on_attach
+            lspconfig[server].setup(config)
+        end
+    end,
 }
